000f9a9b688d2b4d8156fbaae6c6adae1ee89200
Carlos Manuel Soares <carlos@cmpsoares.com>
2021-12-23 19:22:43 +0000

    trying to enable consul connect feature

--- images/packer-nomad-consul-ami/setup_nomad_consul.sh
+++ images/packer-nomad-consul-ami/setup_nomad_consul.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Environment variables are set by packer
-/tmp/terraform-aws-nomad/modules/install-nomad/install-nomad --version "${NOMAD_VERSION}"
-
-git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
-/tmp/terraform-aws-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
+/tmp/terraform-aws-nomad/modules/install-nomad/install-nomad --version "${NOMAD_VERSION}" --user "root"
+/tmp/terraform-aws-nomad/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
+/tmp/terraform-aws-nomad/modules/install-cni-plugins/install-cni-plugins
